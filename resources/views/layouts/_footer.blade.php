1d8bdaa16cfb1e7ff3d22c9fa8aeb613a06c9bd0
root <root@homestead>
2017-03-23 07:32:13 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="/images/iq7WQc2iuW.png">
+      <a href="http://estgroupe.com/">
+        优帆远扬 | 创造不息，交付不止
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
